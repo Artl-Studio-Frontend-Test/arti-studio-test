4b568d00657cffa8904602ce9dfe4b654c2b3a11
WalaaAtiah <walaaatiah97@gmail.com>
2023-03-01 20:31:21 +0000

    style:to main carousel file

--- app/components/MainCarousel.js
+++ app/components/MainCarousel.js
@@ -12,11 +12,11 @@ export default function MainCarousel(props) {
             <div className={style.slider}>
                 <div className={style.slide_track}>
                     {data.map((item, i) => (
-                        <div className="group hover:mx-5 hover:shadow-lg hover:w-[14rem] hover:shadow-slate-300 hover:h-[17rem] hover:mt-[-3rem]   w-[13rem] text-left bg-white rounded-lg mx-4   ">
-                            <div className=" flex justify-center m-2 rounded-lg group-hover:h-[12rem]  " style={{ backgroundColor: `${item.color}` }} >
+                        <div className="group hover:mx-5 hover:shadow-lg hover:w-[14rem] hover:shadow-slate-300 hover:h-[15rem] hover:mt-[-3rem]   w-[13rem] text-left bg-white rounded-lg mx-4   ">
+                            <div className=" flex justify-center m-2 rounded-lg group-hover:h-[10rem]  " style={{ backgroundColor: `${item.color}` }} >
 
                                 <img variant="top" src={`/assets/${item.image}`}
-                                    className={`${style.slide} group-hover:h-[12rem] group-hover:mt-[-1rem] group-hover:mb-[1rem]  group-hover:transform  group-hover:-translate-y-2 group-hover:absolute`} />
+                                    className={`${style.slide} group-hover:h-[12rem] group-hover:mt-[-3rem] group-hover:mb-[3rem]  group-hover:transform  group-hover:-translate-y-2 group-hover:absolute`} />
                             </div>
                             <section className="mt-2 mb-4 ">
                                 <h1 className="text-[#1b1b1b] font-bold ml-4">{item.name}</h1>
